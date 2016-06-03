c300e4596d7170a52d1561d280e8f01e51daad9a
Alan Denison <alan79td@gmail.com>
2016-06-03 10:10:25 -0700

    create branching logic for options to appear

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,14 @@ $(document).ready(function() {
   $("form#userOptions").submit(function(event){
     event.preventDefault();
   var ansClimate = $("#climate").val();
-  alert(ansClimate);
+  var ansActivity = $("#activity").val();
+  var ansLanguage = $("#language").val();
+    if (ansClimate === 'mordor' || ansActivity === 'orcs') {
+      $('#mordorMiddle').show();
+    } else if (ansClimate === 'snowy') {
+      $('#aspenCo').show();
+    } else {
+      $('#madridSpain').show()
+    }
   });
 });
